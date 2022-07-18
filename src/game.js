e979a3809fb2d6cec91d826aa6847e60459baa73
fredrikb12 <fredrik.backman12@gmail.com>
2022-07-18 17:52:09 +0200

    Clean up files and edit margin

--- src/game.js
+++ src/game.js
@@ -4,18 +4,8 @@ import "./styles.css";
 
 const game = (() => {
   const player = Player("You");
-  /*player.gameboard.placeShip({ x: 1, y: 1 }, { x: 5, y: 1 });
-  player.gameboard.placeShip({ x: 6, y: 5 }, { x: 8, y: 5 });
-  player.gameboard.placeShip({ x: 2, y: 3 }, { x: 2, y: 5 });
-  player.gameboard.placeShip({ x: 10, y: 8 }, { x: 10, y: 10 });
-  player.gameboard.placeShip({ x: 4, y: 3 }, { x: 4, y: 6 });*/
 
   const comp = Player("Computer");
-  /*comp.gameboard.placeShip({ x: 8, y: 1 }, { x: 4, y: 1 });
-  comp.gameboard.placeShip({ x: 10, y: 5 }, { x: 8, y: 5 });
-  comp.gameboard.placeShip({ x: 2, y: 8 }, { x: 2, y: 5 });
-  comp.gameboard.placeShip({ x: 10, y: 8 }, { x: 10, y: 6 });
-  comp.gameboard.placeShip({ x: 4, y: 3 }, { x: 4, y: 6 });*/
 
   comp.fillPossibleAttack();
 
